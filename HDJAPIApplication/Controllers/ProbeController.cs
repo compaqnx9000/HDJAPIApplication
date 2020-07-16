de2a98618fcefe422a33fb085f9b5b654945744f
X1yoga <X1yoga@DESKTOP-NO9RTVQ>
2020-07-16 20:42:54 +0800

    添加项目文件。

---
+++ HDJAPIApplication/Controllers/ProbeController.cs
@@ -0,0 +1,64 @@
+﻿using HDJAPIApplication.vo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace HDJAPIApplication.Controllers
+{
+    [ApiController]
+    public class ProbeController : ControllerBase
+    {
+        [HttpGet("missile")]
+        public IActionResult Missile()
+        {
+            MissileVO missileVO = new MissileVO();
+
+            double lng = 116.40;
+            double lat = 39.90;
+            double yield = 300000;
+            double alt = 0;
+
+            // TODO: alt=1000,airblast=3000,=0
+
+            MyCore.MyAnalyse analyse = new MyCore.MyAnalyse();
+
+            missileVO.thermalradiation.Add(analyse.GetThermalRadiationR(yield / 1000, alt * MyCore.Utils.Const.M2FT, 1.9));
+            missileVO.thermalradiation.Add(analyse.GetThermalRadiationR(yield / 1000, alt * MyCore.Utils.Const.M2FT, 6.2));
+            missileVO.thermalradiation.Add(analyse.GetThermalRadiationR(yield / 1000, alt * MyCore.Utils.Const.M2FT, 11.3));
+
+            missileVO.airblast.Add(analyse.CalcShockWaveRadius(yield / 1000, alt * MyCore.Utils.Const.M2FT, 1));
+            missileVO.airblast.Add(analyse.CalcShockWaveRadius(yield / 1000, alt * MyCore.Utils.Const.M2FT, 20));
+            missileVO.airblast.Add(analyse.CalcShockWaveRadius(yield / 1000, alt * MyCore.Utils.Const.M2FT, 3000));
+
+            missileVO.nuclearradiation.Add(analyse.CalcNuclearRadiationRadius(yield / 1000, alt * MyCore.Utils.Const.M2FT, 100));
+            missileVO.nuclearradiation.Add(analyse.CalcNuclearRadiationRadius(yield / 1000, alt * MyCore.Utils.Const.M2FT, 600));
+            missileVO.nuclearradiation.Add(analyse.CalcNuclearRadiationRadius(yield / 1000, alt * MyCore.Utils.Const.M2FT, 5000));
+
+            missileVO.nuclearpulse.Add(analyse.CalcNuclearPulseRadius(yield, alt / 1000, 200.0) * 1000);
+            missileVO.nuclearpulse.Add(analyse.CalcNuclearPulseRadius(yield, alt / 1000, 2000.0) * 1000);
+            missileVO.nuclearpulse.Add(analyse.CalcNuclearPulseRadius(yield, alt / 1000, 50000.0) * 1000);
+
+            double maximumDownwindDistance = 0;double maximumWidth = 0;
+            analyse.CalcRadioactiveFalloutRegion(lng, lat, alt * MyCore.Utils.Const.M2FT, yield / 1000,
+                                                 15, 225, MyCore.enums.DamageEnumeration.Light,
+                                                ref maximumDownwindDistance, ref maximumWidth);
+            missileVO.fallout.Add(maximumDownwindDistance*1000);
+
+            analyse.CalcRadioactiveFalloutRegion(lng, lat, alt * MyCore.Utils.Const.M2FT, yield / 1000,
+                                                 15, 225, MyCore.enums.DamageEnumeration.Heavy,
+                                                ref maximumDownwindDistance, ref maximumWidth);
+            missileVO.fallout.Add(maximumDownwindDistance * 1000);
+
+            analyse.CalcRadioactiveFalloutRegion(lng, lat, alt * MyCore.Utils.Const.M2FT, yield / 1000,
+                                                 15, 225, MyCore.enums.DamageEnumeration.Destroy,
+                                                ref maximumDownwindDistance, ref maximumWidth);
+            missileVO.fallout.Add(maximumDownwindDistance * 1000);
+
+            return new JsonResult(new
+            {
+                return_status = 0,
+                return_msg = "",
+                return_data = missileVO
+            });
+        }
+    }
+}
---
+++ HDJAPIApplication/vo/MissileVO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HDJAPIApplication.vo
+{
+    public class MissileVO
+    {
+        public MissileVO()
+        {
+            thermalradiation = new List<double>();
+            airblast = new List<double>();
+            nuclearradiation = new List<double>();
+            nuclearpulse = new List<double>();
+            fallout = new List<double>();
+        }
+
+        /// <summary>
+        /// 型号。
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// 探测手段。
+        /// </summary>
+        public string Method { get; set; }
+        /// <summary>
+        /// 部队。
+        /// </summary>
+        public string Troop { get; set; }
+        /// <summary>
+        /// 飞行时长。
+        /// </summary>
+        public double FlightTime{get;set;}
+        /// <summary>
+        /// 信息来源。
+        /// </summary>
+        public string Base { get; set; }
+        /// <summary>
+        /// 当前经度。
+        /// </summary>
+        public double FlightLon { get; set; }
+        /// <summary>
+        /// 当前纬度。
+        /// </summary>
+        public double FlightLat { get; set; }
+        /// <summary>
+        /// 当前高程。
+        /// </summary>
+        public double FlightAlt { get; set; }
+        /// <summary>
+        /// 落点经度。
+        /// </summary>
+        public double LaunchLon { get; set; }
+        /// <summary>
+        /// 落点纬度。
+        /// </summary>
+        public double LaunchLat { get; set; }
+        /// <summary>
+        /// 发射时间。
+        /// </summary>
+        public double LaunchTime { get; set; }
+        // <summary>
+        /// 探测时间。
+        /// </summary>
+        public double ProbeTime { get; set; }
+        public List<double> thermalradiation { get; set; }
+        public List<double> airblast { get; set; }
+        public List<double> nuclearradiation { get; set; }
+        public List<double> nuclearpulse { get; set; }
+        public List<double> fallout { get; set; }
+
+
+    }
+}
